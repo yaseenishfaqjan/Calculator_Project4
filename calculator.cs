99bc1ebcd300c1eb531c87029ca65ab9c68807a0
yaseenishfaqjan <yasinishfaq5@gmail.com>
2025-03-06 14:11:38 +0500

    Update calculator

--- calculator.cs
+++ calculator.cs
@@ -12,7 +12,7 @@ class Calculator
 
 static int division(int a, int b) { return a / b; }
 
-static int multiply(int a, int b) { return a * b; }
+static int multiplyyyy(int a, int b) { return a * b; }
 
 
 static int subtraction(int a, int b) { return a - b; }
